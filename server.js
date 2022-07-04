9a98db7325ce804023349461c1a7045966cdf6e7
Paul Cabezal <paulcabezal@gmail.com>
2022-07-04 15:16:15 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,93 @@
+
+console.log('May the Node be with you')
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb+srv://pcabezal:Overmind8@cluster0.xdpij.mongodb.net/?retryWrites=true&w=majority', {
+    useUnifiedTopology: true })
+                    
+    .then(client => {
+        console.log('connected total mongo yo')
+        const db = client.db('starWars')
+        const quotesCollection = db.collection('quotes')
+        // ========================
+        // Middlewares
+        // ========================
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.json())
+        app.use(express.static('public'))
+
+        // app.get('/', (req, res) => {
+        //     const cursor = quotesCollection.find()
+        //     console.log(cursor)
+        // })
+        app.get('/', (req, res) => {
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    res.render('index.ejs', {quotes: results})
+                })
+                .catch(error => console.log(error))
+            
+        })
+
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+              .then(result => {
+                res.redirect('/')
+              })
+              .catch(error => console.error(error))
+          })
+
+          app.put('/quotes', (req, res) => {
+            quotesCollection.findOneAndUpdate(
+                { name: 'Yoda' },
+                {
+                  $set: {
+                    name: req.body.name,
+                    quote: req.body.quote
+                  }
+                },
+                {
+                  upsert: true
+                }
+              )
+                .then(result => {
+                    res.json('Success')
+                })
+                .catch(error => console.error(error))
+          })
+
+          app.delete('/quotes', (req, res) => {
+            quotesCollection.deleteOne(
+              { name: req.body.name }
+            )
+            .then(result => {
+                if (result.deletedCount === 0) {
+                    return res.json('No quote to delete')
+                  }
+                res.json(`Deleted Darth Vadar's quote`)
+              })
+              .catch(error => console.error(error))
+          })
+
+        app.listen()
+    })
+    .catch(error => console.error(error))
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function() {
+    console.log('andre on 3000 yo')
+})
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
+
+
